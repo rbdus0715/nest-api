46aac2b0e9c207273e89ad4f5a1736f78c3b8005
GyuYeon Jo <rbdus0715@gmail.com>
2024-05-22 17:28:40 +0900

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,28 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './apis/products/products.module';
 import { ProductsCategoryModule } from './apis/productsCategories/productsCategories.module';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
     ProductsModule,
     ProductsCategoryModule,
 
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/commons/graphql/schema.gql',
     }),
     TypeOrmModule.forRoot({
-        type: 'mysql',
-        host: 'localhost',       
-        port: 3306,
-        username: 'root',
-        password: 'dmadkr0715',     
-        database: 'myproject',
+        type: process.env.DATABASE_TYPE as 'mysql',
+        host: process.env.DATABASE_HOST,       
+        port: Number(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_HOST,
+        password: process.env.DATABASE_PASSWORD,     
+        database: process.env.DATABASE_DATABASE,
         entities: [__dirname + '/apis/**/*.entity.*'],        
         synchronize: true,     
         logging: true,
